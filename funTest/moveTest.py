2112de93fa8a228bdfe7e9db4345960d1cd14ce9
Gavinwgq <henuboy@live.com>
2020-03-02 20:54:47 +0800

    汉诺塔简单实现

---
+++ funTest/moveTest.py
@@ -0,0 +1,11 @@
+# 汉诺塔递归实现
+def move(n, a, b, c):
+    if n == 1:
+        print(a, "-->", c)
+    else:
+        move(n - 1, a, c, b)
+        print(a, "-->", c)
+        move(n - 1, b, a, c)
+
+
+print(move(4, 'A', 'B', 'C'))
